f4707279ff40aada2f959dbfd9f08b05bb2b0ef7
User <sandymusyafit11@gmail.com>
2025-08-12 10:11:13 +0700

    Update website with latest changes

--- app/berita/donor-darah/page.js
+++ app/berita/donor-darah/page.js
@@ -13,7 +13,7 @@ export default function DonorDarahPage() {
       <img 
         src="/donor-darah3.jpg" 
         alt="Donor Darah" 
-        className="w-full max-w-xl rounded-lg shadow-lg mb-4"
+        className="w-full max-w-xl rounded-lg shadow-lg mb-4 mx-auto"
       />
       <p className="mb-4 text-justify">
         Kepala PMI Kotawaringin Timur dalam sambutannya sangat mengapresiasi kegiatan donor darah ini, dan berharap kegiatan serupa dapat terus dilaksanakan setiap tahunnya, karena dapat memberikan manfaat yang banyak bagi masyarakat dan memberi efek Kesehatan juga bagi para pendonor. Kegiatan aksi donor darah ini didukung oleh karyawan dan karyawati dari anak Perusahaan KLK Group yang berada di Kotawaringin Timur,  serta karyawan dari kontraktor yang berada dilingkungan Perusahaan. Pada kegiatan ini berhasil mengumpulkan 100 Pendaftar, yang kemudian dilanjutkan dengan pemeriksaan persyaratan sebagai pendonor, dimana dari hasil Skrining di dapat 75 orang peserta yang memenuhi syarat sebagai pendonor.
@@ -21,17 +21,17 @@ export default function DonorDarahPage() {
       <img 
         src="/donor-darah1.jpg" 
         alt="Donor Darah" 
-        className="w-full max-w-xl rounded-lg shadow-lg mb-4"
+        className="w-full max-w-xl rounded-lg shadow-lg mb-4 mx-auto"
       />
       <img 
         src="/donor-darah5.jpg" 
         alt="Donor Darah" 
-        className="w-full max-w-xl rounded-lg shadow-lg mb-4"
+        className="w-full max-w-xl rounded-lg shadow-lg mb-4 mx-auto"
       />
       <img 
         src="/donor-darah2.jpg" 
         alt="Donor Darah" 
-        className="w-full max-w-xl rounded-lg shadow-lg mb-4"
+        className="w-full max-w-xl rounded-lg shadow-lg mb-4 mx-auto"
       />
       <p className="mb-4 text-justify">
         Sesuai dengan Tema Hari Donor Darah Sedunia 2025 adalah "Give blood, give hope: together we save lives", yang dalam bahasa Indonesia berarti "Berikan darah, berikan harapan: bersama kita selamatkan nyawa", dikutip dari berbagai sumber. Tema ini menekankan pentingnya donor darah sukarela dalam memberikan harapan dan menyelamatkan nyawa, serta mengajak masyarakat untuk bersatu dalam upaya kemanusiaan ini serta memupuk rasa solidaritas antar sesama, sehingga dengan kegiatan ini diharapkan dapat memberikan manfaat kepada masyarakat terutama masyarakat yang sedang membutuhkan darah.
@@ -39,7 +39,7 @@ export default function DonorDarahPage() {
       <img 
         src="/donor-darah4.jpg" 
         alt="Donor Darah" 
-        className="w-full max-w-xl rounded-lg shadow-lg mb-4"
+        className="w-full max-w-xl rounded-lg shadow-lg mb-4 mx-auto"
         />
          <p className="mb-4 text-justify">
        Kantong darah yang sudah terkumpul langsung diserahkan ke petugas PMI dan segera dibawa ke kantor PMI Kota Sampit.

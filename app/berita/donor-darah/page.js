506bff0755d77148d3d6ceba62a1059a06ebd504
User <sandymusyafit11@gmail.com>
2025-08-12 09:42:59 +0700

    Update website with latest changes

--- app/berita/donor-darah/page.js
+++ app/berita/donor-darah/page.js
@@ -11,7 +11,7 @@ export default function DonorDarahPage() {
         Sebagai salah satu wujud cinta tanah air dan peduli terhadap sesama, PT. Mulia Agro Permai di Sampit bekerja sama dengan Palang Merah Indonesia (PMI) di Kabupaten Kotawaringin Timur melakukan Kegiatan Donor Darah. Acara berlangsung pada hari Sabtu, 14 Juni 2025 bertempat di Klinik Pratama PT. Mulia Agro Permai. Kegiatan donor darah ini merupakan CSR Serentak dari seluruh KLK Group dalam rangka memperingati hari donor darah sedunia.
       </p>
       <img 
-        src="/donor-darah2.jpg" 
+        src="/donor-darah3.jpg" 
         alt="Donor Darah" 
         className="w-full max-w-xl rounded-lg shadow-lg mb-4"
       />
@@ -19,7 +19,7 @@ export default function DonorDarahPage() {
         Kepala PMI Kotawaringin Timur dalam sambutannya sangat mengapresiasi kegiatan donor darah ini, dan berharap kegiatan serupa dapat terus dilaksanakan setiap tahunnya, karena dapat memberikan manfaat yang banyak bagi masyarakat dan memberi efek Kesehatan juga bagi para pendonor. Kegiatan aksi donor darah ini didukung oleh karyawan dan karyawati dari anak Perusahaan KLK Group yang berada di Kotawaringin Timur,  serta karyawan dari kontraktor yang berada dilingkungan Perusahaan. Pada kegiatan ini berhasil mengumpulkan 100 Pendaftar, yang kemudian dilanjutkan dengan pemeriksaan persyaratan sebagai pendonor, dimana dari hasil Skrining di dapat 75 orang peserta yang memenuhi syarat sebagai pendonor.
       </p>
       <img 
-        src="/donor-darah4.jpg" 
+        src="/donor-darah2.jpg" 
         alt="Donor Darah" 
         className="w-full max-w-xl rounded-lg shadow-lg mb-4"
       />
@@ -27,7 +27,7 @@ export default function DonorDarahPage() {
         Sesuai dengan Tema Hari Donor Darah Sedunia 2025 adalah "Give blood, give hope: together we save lives", yang dalam bahasa Indonesia berarti "Berikan darah, berikan harapan: bersama kita selamatkan nyawa", dikutip dari berbagai sumber. Tema ini menekankan pentingnya donor darah sukarela dalam memberikan harapan dan menyelamatkan nyawa, serta mengajak masyarakat untuk bersatu dalam upaya kemanusiaan ini serta memupuk rasa solidaritas antar sesama, sehingga dengan kegiatan ini diharapkan dapat memberikan manfaat kepada masyarakat terutama masyarakat yang sedang membutuhkan darah.
       </p>
       <img 
-        src="/donor-darah3.jpg" 
+        src="/donor-darah4.jpg" 
         alt="Donor Darah" 
         className="w-full max-w-xl rounded-lg shadow-lg mb-4"
         />
